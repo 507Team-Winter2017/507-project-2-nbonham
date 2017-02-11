4851139d54e665e2d908e59bd0e336d0dabd3b67
Nate Bonham <nbonham@umich.edu>
2017-02-11 11:32:02 -0500

    finished part 4

--- proj2.py
+++ proj2.py
@@ -1,5 +1,6 @@
 #proj2.py
 from bs4 import BeautifulSoup
+import re
 
 from urllib.request import urlopen
 import ssl
@@ -81,3 +82,57 @@ print('\n*********** PROBLEM 4 ***********')
 print("UMSI faculty directory emails\n")
 
 ### Your Problem 4 solution goes here
+contactURLs = []
+emails = []
+count = 1
+pagenation = 1
+umichURL = 'https://www.si.umich.edu'
+
+url = 'https://www.si.umich.edu/directory?field_person_firstname_value=&field_person_lastname_value=&rid=4'
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+## get all the contact details urls on one page in a list
+#<div class="field-item even"><a href="/node/9915">Contact Details</a>
+#<a href="/node/9915">Contact Details</a>
+
+def getSlugs(s) :
+    contactSlugs = soup.findAll(href=re.compile('node'),class_="")
+
+    for slug in contactSlugs :
+        contactURLs.append(umichURL + slug.get('href'))
+
+getSlugs(soup)
+
+## get all the contact details for all 6 pages in the same list (can hard code, but will need to actually do the crawling)
+#https://www.si.umich.edu/directory?field_person_firstname_value=&field_person_lastname_value=&rid=4&page=4
+
+while pagenation < 6:
+
+    url = 'https://www.si.umich.edu/directory?field_person_firstname_value=&field_person_lastname_value=&rid=4&page=' + str(pagenation)
+    pagenation += 1
+    html = urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, "html.parser")
+
+    getSlugs(soup)
+
+## iterate through the list and open each url in a for loop
+## extract the email out of the approriate location and store that in a list
+for contact in contactURLs :
+    url = contact
+    html = urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, "html.parser")
+
+    #<a href="mailto:shevonad@umich.edu">shevonad@umich.edu</a>
+    facultyEmail = soup.findAll(href=re.compile('mailto'))
+
+    for e in facultyEmail :
+        emails.append(e.get_text())
+
+    #print((facultyEmail))
+    #emails.append(facultyEmail.get_text())
+
+## print out the list of emails with a number before the email like 1 ackerm@umich.edu
+for email in emails :
+    print (str(count) + " " + email)
+    count += 1
